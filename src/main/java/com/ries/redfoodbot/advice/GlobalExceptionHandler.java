fbc674c825cf782a6d3e73460f19223900ff10c3
ries0703 <117291686+Ries0703@users.noreply.github.com>
2024-08-09 19:06:10 +0700

    refactor config vars

--- src/main/java/com/ries/redfoodbot/advice/GlobalExceptionHandler.java
+++ src/main/java/com/ries/redfoodbot/advice/GlobalExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.ries.redfoodbot.advice;
 
-import com.ries.redfoodbot.exceptions.WebHookException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
--- src/main/java/com/ries/redfoodbot/service/impl/WebHookServiceImpl.java
+++ src/main/java/com/ries/redfoodbot/service/impl/WebHookServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ries.redfoodbot.service.impl;
 
-import com.google.gson.annotations.JsonAdapter;
 import com.ries.redfoodbot.service.FacebookService;
 import com.ries.redfoodbot.service.WebHookService;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +15,10 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class WebHookServiceImpl implements WebHookService {
 
-
     private static final Logger logger = LoggerFactory.getLogger(WebHookServiceImpl.class);
+
     private final FacebookService facebookService;
+
     @Value("${facebook.verify-token}")
     private String verifyToken;
 
@@ -31,24 +31,20 @@ public class WebHookServiceImpl implements WebHookService {
     }
 
     @Override
-    public void handleWebHookEvent(Map<String, Object> body) throws Exception {
+    @SuppressWarnings("unchecked")
+    public void handleWebHookEvent(Map<String, Object> body) {
         List<Map<String, Object>> entries = (List<Map<String, Object>>) body.get("entry");
         for (Map<String, Object> entry : entries) {
             // Get the first messaging event in the entry
             List<Map<String, Object>> messaging = (List<Map<String, Object>>) entry.get("messaging");
             if (messaging != null && !messaging.isEmpty()) {
                 Map<String, Object> webhookEvent = messaging.get(0);
-                System.out.println(webhookEvent);
-
                 // Get the sender PSID
                 String senderPsId = (String) ((Map<String, Object>) webhookEvent.get("sender")).get("id");
-                logger.info("Sender PSID: {}", senderPsId);
-
+                logger.info("webhook even: {}", webhookEvent);
                 if (webhookEvent.containsKey("message")) {
-                    // Construct the response message
                     Map<String, Object> responseMessage = Map.of("text", "Thank you for your message!");
                     logger.info(responseMessage.toString());
-                    // Call the Facebook service to send the response
                     facebookService.sendResponseToUser(senderPsId, responseMessage);
                 } else {
                     System.out.println("Received non-message event: " + webhookEvent);
